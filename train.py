83d895330f631be201e4434c34a5cb2681f66f0e
Osman Seferoglu <timecreate@icloud.com>
2024-08-07 19:51:09 +0300

    chore: commented device in train.py for better understanding it's usage

--- train.py
+++ train.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8n.yaml')
 
-
+# change device to '0' for CPU ,'1' for GPU and 'mps' for M1 chips
 results = model.train(data='/Users/osman/Documents/GitHub/yabanitestpit2/config.yaml', epochs=5, device='mps',batch=16, workers=8)
